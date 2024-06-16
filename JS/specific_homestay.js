ea3e45ad4ccf721dc729bcfc688a0f0a59bd2a6e
nguyenthanhkhoi30 <151111403+nguyenthanhkhoi30@users.noreply.github.com>
2024-06-17 02:09:27 +0700

    Add files via upload

---
+++ JS/specific_homestay.js
@@ -0,0 +1,190 @@
+const DATAS = [
+  {
+    id: 1,
+    name: "Le Tuan Minh Hotel",
+    rate: 1,
+    address:
+      "https://www.google.com/maps/place/L%C3%AA+Tu%E1%BA%A5n+Minh+Hotel/@11.9387107,108.4316227,17z/data=!4m9!3m8!1s0x3171132e032285c1:0xb326def9fcbbf065!5m2!4m1!1i2!8m2!3d11.9387107!4d108.4341976!16s%2Fg%2F11hc_c_jjj?hl=vi-VN&entry=tts&g_ep=EgoyMDI0MDYwNS4wKgBIAVAD",
+    description:
+      "Nằm cách Vườn hoa Đà Lạt 2.6 km và Sân golf Dalat Palace Golf Club 3 km, Le Tuan Minh Hotel cung cấp các phòng ở Đà Lạt.",
+    full_description:
+      "Bạn đủ điều kiện nhận giảm giá Genius tại Le Tuan Minh Hotel! Để tiết kiệm tại chỗ nghỉ này, bạn chỉ cần đăng nhập. Nằm cách Vườn hoa Đà Lạt 2.6 km và Sân golf Dalat Palace Golf Club 3 km, Le Tuan Minh Hotel cung cấp các phòng ở Đà Lạt. Với phòng chờ chung, khách sạn nằm gần một số điểm tham quan nổi tiếng, cách Quảng trường Lâm Viên 17 phút đi bộ, Hồ Xuân Hương 1.6 km và Công viên Yersin 1.8 km. Chỗ nghỉ cung cấp lễ tân 24/24, dịch vụ đưa đón sân bay, bếp chung và Wi-Fi miễn phí.",
+    price: 855000,
+    image: "../RES/tc6.jpg",
+    image_blog: "../RES/6.jpg",
+    image_html_ref: "hinh_XP_Le_Tuan_Minh.html",
+  },
+  {
+    id: 2,
+    name: "Miền Nhiệt Đới 2 Hotel",
+    rate: 1,
+    address:
+      "https://www.google.com/maps/place/Kh%C3%A1ch+S%E1%BA%A1n+Mi%E1%BB%81n+Nhi%E1%BB%87t+%C4%90%E1%BB%9Bi+2/@11.940548,108.4313035,17z/data=!4m9!3m8!1s0x3171132e798cef81:0x208dcd6fb5e8d53b!5m2!4m1!1i2!8m2!3d11.940548!4d108.4338784!16s%2Fg%2F11gzgywq6?hl=vi-VN&entry=ttu",
+    description:
+      "Nằm ở Đà Lạt, cách Quảng trường Lâm Viên 17 phút đi bộ và Hồ Xuân Hương 1.6 km, Miền Nhiệt Đới 2 Hotel cung cấp chỗ nghỉ có phòng chờ chung và Wi-Fi miễn phí ở toàn bộ chỗ nghỉ cũng như chỗ đậu xe...",
+    full_description:
+      "Bạn đủ điều kiện nhận giảm giá Genius tại Miền Nhiệt Đới 2 Hotel! Để tiết kiệm tại chỗ nghỉ này, bạn chỉ cần đăng nhập. Nằm ở Đà Lạt, cách Quảng trường Lâm Viên 17 phút đi bộ và Hồ Xuân Hương 1.6 km, Miền Nhiệt Đới 2 Hotel cung cấp chỗ nghỉ có phòng chờ chung và Wi-Fi miễn phí ở toàn bộ chỗ nghỉ cũng như chỗ đậu xe riêng miễn phí cho khách lái xe. Khách sạn 2 sao này có dịch vụ phòng và quầy lễ tân 24 giờ. Chỗ nghỉ này cung cấp dịch vụ tiền sảnh, bàn bán tour và dịch vụ thu đổi ngoại tệ cho khách. Tất cả các phòng đi kèm TV màn hình phẳng có truyền hình vệ tinh, tủ lạnh, ấm đun nước, vòi xịt/chậu rửa vệ sinh, đồ vệ sinh cá nhân miễn phí và bàn làm việc. Ngoài phòng tắm riêng với vòi sen và máy sấy tóc, một số phòng tại khách sạn cũng có view thành phố. Tại Miền Nhiệt Đới 2 Hotel, các phòng đều có ga trải giường và khăn tắm. Chỗ nghỉ cách Công viên Yersin 1.8 km và Vườn hoa Đà Lạt 2.6 km. Sân bay gần nhất là Sân bay Liên Khương, cách Miền Nhiệt Đới 2 Hotel 28 km, đồng thời chỗ nghỉ có cung cấp dịch vụ đưa đón sân bay mất phí.",
+    price: 631750,
+    image: "../RES/tc7.jpg",
+    image_blog: "../RES/2.jpg",
+    image_html_ref: "hinh_XP_mienNhietDoi.html",
+  },
+  {
+    id: 3,
+    name: "Moc Tra Hotel",
+    rate: 1,
+    address:
+      "https://www.google.com/maps/place/Kh%C3%A1ch+s%E1%BA%A1n+M%E1%BB%99c+Tr%C3%A0/@11.9339145,108.4280213,15z/data=!4m9!3m8!1s0x3171133220dffc35:0x38b5085c7238f5f8!5m2!4m1!1i2!8m2!3d11.9367843!4d108.433312!16s%2Fg%2F11gfgsd9c3?hl=vi-VN&entry=ttu",
+    description:
+      "Tọa lạc ở Đà Lạt, cách Quảng trường Lâm Viên 19 phút đi bộ, Moc Tra Hotel cung cấp chỗ nghỉ có phòng chờ chung, chỗ đậu xe riêng miễn phí và sân hiên.",
+    full_description:
+      "Bạn đủ điều kiện nhận giảm giá Genius tại Moc Tra Hotel! Để tiết kiệm tại chỗ nghỉ này, bạn chỉ cần đăng nhập. Tọa lạc ở Đà Lạt, cách Quảng trường Lâm Viên 19 phút đi bộ, Moc Tra Hotel cung cấp chỗ nghỉ có phòng chờ chung, chỗ đậu xe riêng miễn phí và sân hiên. Khách sạn 1 sao này có dịch vụ tiền sảnh và bàn bán tour. Chỗ nghỉ cung cấp lễ tân 24/24, dịch vụ đưa đón sân bay, dịch vụ phòng và Wi-Fi miễn phí ở toàn bộ chỗ nghỉ. Các phòng tại khách sạn được trang bị khu vực ghế ngồi, TV màn hình phẳng có truyền hình cáp, phòng tắm riêng, đồ vệ sinh cá nhân miễn phí và vòi xịt/chậu rửa vệ sinh. Mỗi phòng đều được thiết kế có ấm đun nước, trong đó một số phòng có ban công. Tại Moc Tra Hotel, phòng nào cũng có ga trải giường và khăn tắm.",
+    price: 390000,
+    image: "../RES/tc10.webp",
+    image_blog: "../RES/9.jpg",
+    image_html_ref: "hinh_XP_Moc_Tra.html",
+  },
+  {
+    id: 4,
+    name: "Dalat Wind Hotel",
+    rate: 2,
+    address:
+      "https://www.google.com/maps/place/Dalat+Wind+Hotel/@11.9422051,108.443728,14z/data=!4m9!3m8!1s0x3171134f1124694f:0xacbdd6d21d69802e!5m2!4m1!1i2!8m2!3d11.9476907!4d108.4413053!16s%2Fg%2F11v195tzw7?hl=vi-VN&entry=ttu",
+    description:
+      "Tọa lạc ở Đà Lạt, cách Quảng trường Lâm Viên 18 phút đi bộ, Nature Hotel cung cấp chỗ nghỉ có khu vườn, chỗ đậu xe riêng miễn phí và sân hiên. Khách sạn 3 sao này có dịch vụ tiền sảnh và bàn bán tour....",
+    full_description:
+      "Bạn đủ điều kiện nhận giảm giá Genius tại Dalat Wind Hotel! Để tiết kiệm tại chỗ nghỉ này, bạn chỉ cần đăng nhập. Tọa lạc tại thành phố Đà Lạt, cách Hồ Xuân Hương 500 m, Dalat Wind Hotel là khách sạn 2 sao có lễ tân 24 giờ, sảnh khách chung, Wi-Fi và chỗ đỗ xe riêng miễn phí. Tại khách sạn, tất cả các phòng đều có bàn làm việc, TV màn hình phẳng, ấm đun nước và phòng tắm riêng với chậu rửa vệ sinh (bidet). Một số phòng còn có ban công. Khách sạn phục vụ bữa sáng gọi món hàng ngày. Du khách có thể dùng bữa tại nhà hàng trong khuôn viên, nơi chuyên phục vụ các món nướng/BBQ. Du khách cũng có thể thư giãn trên sân hiên tắm nắng.",
+    price: 257452,
+    image: "../RES/tc2.jpg",
+    image_blog: "../RES/3.jpg",
+    image_html_ref: "hinh_XP_dalat_wind.html",
+  },
+  {
+    id: 5,
+    name: "GreenMeadow Central Hotel Đà Lạt",
+    rate: 2,
+    address:
+      "https://www.google.com/maps/place/GreenMeadow+Hotel+DaLat/@11.9342453,108.424771,17z/data=!4m9!3m8!1s0x317113334c1bb2bf:0x21fa1a1622d021de!5m2!4m1!1i2!8m2!3d11.9342453!4d108.4273459!16s%2Fg%2F11h0mwv643?hl=vi-VN&entry=ttu",
+    description:
+      "Tọa lạc ở Đà Lạt, cách Quảng trường Lâm Viên 2.9 km, GreenMeadow Central Hotel Đà Lạt cung cấp chỗ nghỉ có khu vườn, chỗ đậu xe riêng miễn phí, phòng chờ chung và quầy bar.",
+    full_description:
+      "Bạn đủ điều kiện nhận giảm giá Genius tại GreenMeadow Central Hotel Đà Lạt! Để tiết kiệm tại chỗ nghỉ này, bạn chỉ cần đăng nhập. Tọa lạc ở Đà Lạt, cách Quảng trường Lâm Viên 2.9 km, GreenMeadow Central Hotel Đà Lạt cung cấp chỗ nghỉ có khu vườn, chỗ đậu xe riêng miễn phí, phòng chờ chung và quầy bar. Khách sạn 2 sao này cung cấp dịch vụ phòng, quầy lễ tân 24 giờ và Wi-Fi miễn phí. Một số căn tại chỗ nghỉ có ban công với view núi. Tại khách sạn, mỗi phòng đều được thiết kế có bàn làm việc, TV màn hình phẳng, phòng tắm riêng, ga trải giường và khăn tắm. Các phòng đều có ấm đun nước, trong đó một số phòng có sân hiên và một số khác thì nhìn ra thành phố. Phòng khách đều có tủ lạnh. GreenMeadow Central Hotel Đà Lạt cách Hồ Xuân Hương 3.1 km và Công viên Yersin 3.2 km. Sân bay gần nhất là Sân bay Liên Khương, cách chỗ nghỉ 29 km.",
+    price: 275000,
+    image: "../RES/tc5.jpg",
+    image_blog: "../RES/5.jpg",
+    image_html_ref: "hinh_XP_GreenMeadow.html",
+  },
+  {
+    id: 6,
+    name: "RAON Bazan Hotel - STAY 24H",
+    rate: 3,
+    address:
+      "https://www.google.com/maps/place/RAON+Bazan+Hotel/@11.9283958,108.4432169,17z/data=!4m9!3m8!1s0x31a1099f8765735f:0x2d2b448202178a8!5m2!4m1!1i2!8m2!3d11.9283958!4d108.4450959!16s%2Fg%2F11vsdfy05g?hl=vi-VN&entry=ttu",
+    description:
+      "Nằm ở Đà Lạt, cách Công viên Yersin 19 phút đi bộ, RAON Bazan Hotel - STAY 24H cung cấp chỗ nghỉ có khu vườn, chỗ đậu xe riêng miễn phí và phòng chờ chung.",
+    full_description:
+      "Bạn đủ điều kiện nhận giảm giá Genius tại RAON Bazan Hotel - STAY 24H! Để tiết kiệm tại chỗ nghỉ này, bạn chỉ cần đăng nhập. Nằm ở Đà Lạt, cách Công viên Yersin 19 phút đi bộ, RAON Bazan Hotel - STAY 24H cung cấp chỗ nghỉ có khu vườn, chỗ đậu xe riêng miễn phí và phòng chờ chung. Khách sạn 3 sao này có dịch vụ phòng và dịch vụ tiền sảnh. Chỗ nghỉ cung cấp lễ tân 24/24, dịch vụ đưa đón sân bay, bếp chung và Wi-Fi miễn phí ở toàn bộ chỗ nghỉ. Tại khách sạn, tất cả các phòng có bàn làm việc, TV màn hình phẳng, phòng tắm riêng, ga trải giường và khăn tắm. Các căn đều có minibar. RAON Bazan Hotel - STAY 24H cách Hồ Xuân Hương 1.8 km và Quảng trường Lâm Viên 1.8 km. Sân bay gần nhất là Sân bay Liên Khương, cách chỗ nghỉ 25 km.",
+    price: 463320,
+    image: "../RES/tc1.jpg",
+    image_blog: "../RES/1.jpg",
+    image_html_ref: "Hinh_XP_raon.html",
+  },
+  {
+    id: 7,
+    name: "Nature Hotel",
+    rate: 3,
+    address:
+      "https://www.google.com/maps/place/Nature+Hotel/@11.931395,108.4364786,15z/data=!4m9!3m8!1s0x317113cf6f9faf91:0x10fb90ebb286f100!5m2!4m1!1i2!8m2!3d11.9322043!4d108.4422776!16s%2Fg%2F11tdnhx42s?hl=vi-VN&entry=ttu",
+    description:
+      "Tọa lạc tại thành phố Đà Lạt, cách Hồ Xuân Hương 500 m, Dalat Wind Hotel là khách sạn 2 sao có lễ tân 24 giờ, sảnh khách chung, Wi-Fi và chỗ đỗ xe riêng miễn phí.",
+    full_description:
+      "Bạn đủ điều kiện nhận giảm giá Genius tại Nature Hotel! Để tiết kiệm tại chỗ nghỉ này, bạn chỉ cần đăng nhập. Tọa lạc ở Đà Lạt, cách Quảng trường Lâm Viên 18 phút đi bộ, Nature Hotel cung cấp chỗ nghỉ có khu vườn, chỗ đậu xe riêng miễn phí và sân hiên. Khách sạn 3 sao này có dịch vụ tiền sảnh và bàn bán tour. Chỗ nghỉ cung cấp lễ tân 24/24, dịch vụ đưa đón sân bay, dịch vụ phòng và Wi-Fi miễn phí ở toàn bộ chỗ nghỉ. Phòng khách tại khách sạn được trang bị khu vực ghế ngồi, TV màn hình phẳng có truyền hình cáp, két an toàn, cùng phòng tắm riêng với vòi xịt/chậu rửa vệ sinh, đồ vệ sinh cá nhân miễn phí và máy sấy tóc. Các phòng đều đi kèm với ấm đun nước, trong đó một số phòng có ban công và một số khác thì nhìn ra thành phố. Tại Nature Hotel, phòng nào cũng có ga trải giường và khăn tắm. Nature Hotel có sân chơi trẻ em.",
+    price: 427329,
+    image: "../RES/tc3.jpg",
+    image_blog: "../RES/4.jpg",
+    image_html_ref: "hinh_XP_Nature_Hotel.html",
+  },
+  {
+    id: 8,
+    name: "Len's Hotel",
+    rate: 3,
+    address:
+      "https://www.google.com/maps/place/Len's+Hotel/@11.9405478,108.4235786,15z/data=!4m9!3m8!1s0x3171133d21fb0bc5:0xad807f5b39c4026!5m2!4m1!1i2!8m2!3d11.947065!4d108.4404485!16s%2Fg%2F11q2ys25z7?hl=vi-VN&entry=ttu",
+    description:
+      "Tọa lạc ở Đà Lạt, cách Quảng trường Lâm Viên 2.9 km, GreenMeadow Central Hotel Đà Lạt cung cấp chỗ nghỉ có khu vườn, chỗ đậu xe riêng miễn phí, phòng chờ chung và quầy bar.",
+    full_description:
+      "Bạn đủ điều kiện nhận giảm giá Genius tại Len's Hotel! Để tiết kiệm tại chỗ nghỉ này, bạn chỉ cần đăng nhập. Tọa lạc ở Đà Lạt, cách Sân golf Dalat Palace Golf Club 1.7 km, Len's Hotel cung cấp chỗ nghỉ có phòng chờ chung, chỗ đậu xe riêng miễn phí, sân hiên và nhà hàng. Khách sạn 3 sao này có bàn bán tour và phòng giữ hành lý. Chỗ nghỉ này cung cấp quầy lễ tân 24 giờ, dịch vụ tiền sảnh và dịch vụ thu đổi ngoại tệ cho khách. Các phòng được trang bị TV màn hình phẳng có truyền hình cáp, tủ lạnh, ấm đun nước, vòi sen, máy sấy tóc và tủ quần áo. Với phòng tắm riêng được trang bị vòi xịt/chậu rửa vệ sinh và đồ vệ sinh cá nhân miễn phí, các phòng tại khách sạn cũng có Wi-Fi miễn phí, trong đó một số phòng có view thành phố. Tại Len's Hotel, tất cả các phòng đều được thiết kế có ga trải giường và khăn tắm. Chỗ nghỉ có phục vụ bữa sáng thực đơn buffet mỗi sáng.",
+    price: 896724,
+    image: "../RES/tc8.webp",
+    image_blog: "../RES/7.jpg",
+    image_html_ref: "hinh_XP_Len's_Hotel.html",
+  },
+  {
+    id: 9,
+    name: "Raon Dalat - STAY 24H",
+    rate: 3,
+    address:
+      "https://www.google.com/maps/place/RAON+Dalat/@11.9453869,108.4221134,14z/data=!4m9!3m8!1s0x31711375be1f8311:0xb336164ff0a39d30!5m2!4m1!1i2!8m2!3d11.9624631!4d108.4404237!16s%2Fg%2F11vzbty5v0?hl=vi-VN&entry=ttu",
+    description:
+      "Tọa lạc ở Đà Lạt, cách Sân golf Dalat Palace Golf Club 1.9 km, Raon Dalat - STAY 24H cung cấp chỗ nghỉ có khu vườn, chỗ đậu xe riêng miễn phí, phòng chờ chung và sân hiên.",
+    full_description:
+      "Bạn đủ điều kiện nhận giảm giá Genius tại Raon Dalat - STAY 24H! Để tiết kiệm tại chỗ nghỉ này, bạn chỉ cần đăng nhập. Tọa lạc ở Đà Lạt, cách Sân golf Dalat Palace Golf Club 1.9 km, Raon Dalat - STAY 24H cung cấp chỗ nghỉ có khu vườn, chỗ đậu xe riêng miễn phí, phòng chờ chung và sân hiên. Khách sạn 3 sao này có dịch vụ phòng và dịch vụ tiền sảnh. Chỗ nghỉ cung cấp lễ tân 24/24, dịch vụ đưa đón sân bay, bếp chung và Wi-Fi miễn phí ở toàn bộ chỗ nghỉ. Các phòng ở khách sạn được trang bị TV màn hình phẳng có truyền hình vệ tinh. Ngoài phòng tắm riêng với vòi sen và đồ vệ sinh cá nhân miễn phí, một số căn tại Raon Dalat - STAY 24H cũng có view thành phố. Tại chỗ nghỉ, tất cả các phòng đều được thiết kế có ga trải giường và khăn tắm. Raon Dalat - STAY 24H cách Vườn hoa Đà Lạt 2.4 km và Hồ Xuân Hương 4.2 km. Sân bay gần nhất là Sân bay Liên Khương, cách khách sạn 31 km.",
+    price: 320000,
+    image: "../RES/tc12.webp",
+    image_blog: "../RES/11.jpg",
+    image_html_ref: "hinh_XP_Raon_Dalat.html",
+  },
+  {
+    id: 10,
+    name: "New Century Dalat Hotel",
+    rate: 4,
+    address:
+      "https://www.google.com/maps/place/New+Century+Dalat+Hotel/@11.9236753,108.4406603,17z/data=!4m9!3m8!1s0x317113e107fce7ff:0xc71b8918d8f2da4b!5m2!4m1!1i2!8m2!3d11.9236753!4d108.4432352!16s%2Fg%2F11v0nqr2n9?hl=vi-VN&entry=ttu",
+    description:
+      "Tọa lạc ở Đà Lạt, cách Quảng trường Lâm Viên 19 phút đi bộ, Moc Tra Hotel cung cấp chỗ nghỉ có phòng chờ chung, chỗ đậu xe riêng miễn phí và sân hiên.",
+    full_description:
+      "Bạn đủ điều kiện nhận giảm giá Genius tại New Century Dalat Hotel! Để tiết kiệm tại chỗ nghỉ này, bạn chỉ cần đăng nhập. Nằm ở Đà Lạt, cách Công viên Yersin 2.8 km và Hồ Xuân Hương 2.8 km, New Century Dalat Hotel cung cấp chỗ nghỉ có nhà hàng và Wi-Fi miễn phí cũng như chỗ đậu xe riêng miễn phí cho khách lái xe. Khách sạn 4 sao này có dịch vụ phòng và quầy lễ tân 24 giờ. Khách sạn có bể sục và phòng giữ hành lý. Tất cả các phòng tại khách sạn đi kèm khu vực ghế ngồi, TV màn hình phẳng có truyền hình cáp, két an toàn, cùng phòng tắm riêng với vòi xịt/chậu rửa vệ sinh, đồ vệ sinh cá nhân miễn phí và máy sấy tóc. Các phòng được thiết kế có ấm đun nước, trong đó một số phòng có ban công và một số khác thì nhìn ra thành phố. Tại New Century Dalat Hotel, các phòng đều có ga trải giường và khăn tắm. Chỗ nghỉ có phục vụ bữa sáng thực đơn buffet mỗi buổi sáng. New Century Dalat Hotel cách Quảng trường Lâm Viên 3 km và Thiền Viện Trúc Lâm 4.5 km. Sân bay gần nhất là Sân bay Liên Khương, cách khách sạn 25 km, đồng thời chỗ nghỉ này cũng cung cấp dịch vụ đưa đón sân bay mất phí.",
+    price: 972000,
+    image: "../RES/tc9.webp",
+    image_blog: "../RES/7.jpg",
+    image_html_ref: "hinh_XP_New_Century.html",
+  },
+];
+
+const convertPriceToVND = (price) => {
+  return price.toLocaleString("it-IT", { style: "currency", currency: "VND" });
+};
+
+const div6 = document.querySelector(".div6");
+console.log(div6);
+
+const dataSelectedId = localStorage.getItem("dataSelectedId");
+if (dataSelectedId) {
+  const data = DATAS.find((item) => item.id === Number(dataSelectedId));
+  const price = convertPriceToVND(data.price);
+  let star = "";
+  for (let i = 0; i < data.rate; i++) {
+    star += `<img src="../RES/star.png" />`;
+  }
+  let content = `<p9><b>${data.name}</p9>
+        <div class="star1">
+            ${star}
+            <br>
+            <p4><a href=${data.address}><i class="fa fa-map-marker" aria-hidden="true"> <p5>36 To Hien Thanh , Đà Lạt, Việt Nam </p5></i></a></p4>
+        </div>
+        <a href=${data.image_html_ref}><img src=${data.image_blog} width="1000" height="500"></a>
+
+        <p11>
+          ${data.full_description}
+        </p11>
+
+        <div class="div8">
+            <p6>Giá từ</p6><br>
+            <p7>${price}</p7><br>
+            <p6>Mỗi đêm</p6>
+            
+            </table> 
+        </div>
+`;
+  div6.insertAdjacentHTML("beforeend", content);
+}
